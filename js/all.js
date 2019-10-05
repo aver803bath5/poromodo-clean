0bce18bacd82ff97254cd5706934276163a6329d
g9Mien <aver803bath261@gmail.com>
2019-10-05 20:13:29 +0800

    Add formatTimerNumber Vue method

--- js/all.js
+++ js/all.js
@@ -5,4 +5,9 @@ const vm = new Vue({
     timerHour: 25,
     timerSecond: 0,
   },
+  methods: {
+    formatTimerNumber(num) {
+      return num < 10 ? `0${num.toString()}` : num.toString();
+    },
+  },
 });
